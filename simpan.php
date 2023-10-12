438e2d28fe996cc04145d6dfeb33a03d7f0c7240
Muhammad Samy El Islamy <147031097+Samyel13@users.noreply.github.com>
2023-10-12 10:35:57 +0700

    matadiklat

---
+++ simpan.php
@@ -0,0 +1,22 @@
+<?php
+if (isset($_POST['simpan'])) {
+    include_once "koneksi.php";
+    $matadiklat   = $_POST['matadiklat'];
+    $sks = $_POST['sks'];
+
+    $sql = "INSERT INTO matadiklat SET 
+    matadiklat='$matadiklat',
+    sks='$sks'";
+    $query = mysqli_query($koneksi, $sql);
+    if ($query) {
+        header('Location:index.php?n=matadiklat&s=tampil');
+        //echo "berhasil";
+    } else {
+        include "index.php?n=matadiklat&s=tampil";
+        echo '<script language="JavaScript">';
+        echo 'alert("Data Gagal Ditambahkan.")';
+        echo '</script>';
+    }
+} else {
+    echo '<script>window.history.back()</script>';
+}
